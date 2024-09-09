f337043de35054887eb657d912589d8e9158dc55
CharisConrado <cmconradoc@ufpso.edu.co>
2024-09-09 14:13:55 -0500

    Update Files

---
+++ EmpresaX/src/empresax/EmpresaX.java
@@ -0,0 +1,13 @@
+
+package empresax;
+
+import empresax.services.EmpleadoService;
+
+public class EmpresaX {
+
+    public static void main(String[] args) {
+        EmpleadoService service = new EmpleadoService();
+        service.menu();
+    }
+    
+}
---
+++ EmpresaX/src/empresax/models/Empleado.java
@@ -0,0 +1,55 @@
+package empresax.models;
+
+public class Empleado {
+    private String documento;
+    private String nombreEmpleado;
+    private int horasTrabajadas;
+    private double valorHora;
+    private double sueldo;
+    
+    public Empleado(){
+        this.documento = "";
+        this.nombreEmpleado = "No definido";
+        this.horasTrabajadas = 0;
+        this.valorHora = 0.0;
+        this.sueldo = 0.0;
+    }
+
+    public Empleado(String documento, String nombreEmpleado, int horasTrabajadas, 
+            double valorHora, double sueldo) {
+        this.documento = documento;
+        this.nombreEmpleado = nombreEmpleado;
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorHora = valorHora;
+        this.sueldo = sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }   
+    
+}
