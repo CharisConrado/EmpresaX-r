3770a0641bdad627fcd6b3de6bc1acef744d77c3
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-09-10 19:51:15 -0500

    UpdateFiles

--- EmpresaX/src/empresax/EmpresaX.java
+++ EmpresaX/src/empresax/EmpresaX.java
@@ -4,7 +4,6 @@ package empresax;
 import empresax.services.EmpleadoService;
 
 public class EmpresaX {
-
     public static void main(String[] args) {
         EmpleadoService service = new EmpleadoService();
         service.menu();
--- EmpresaX/src/empresax/services/EmpleadoService.java
+++ EmpresaX/src/empresax/services/EmpleadoService.java
@@ -1,5 +1,7 @@
 package empresax.services;
+
 import empresax.models.Empleado;
+
 import java.util.Scanner;
 
 public class EmpleadoService {
@@ -9,76 +11,78 @@ public class EmpleadoService {
 
     public EmpleadoService() {
         sc = new Scanner(System.in);
-        crearListaEmpleados();
+        listaEmpleados = new ArrayList<>();
     }
 
-    private void crearListaEmpleados(){
-        System.out.println("Ingrese la cantidad de empleados");
-        int cantidad = sc.nextInt();
-        listaEmpleados = new Empleado[cantidad];
-    }
-    
-    public void menu(){
+
+    public void menu() {
         int opcion;
-        do{
+        do {
             System.out.println("::MENU::");
             System.out.println("1 - CREAR EMPLEADO");
             System.out.println("2 - MODIFICAR EMPLEADO");
             System.out.println("3 - BUSCAR EMPLEADO");
             System.out.println("4 - LISTAR EMPLEADOS");
+            System.out.println("5 - ELIMINAR EMPLEADO");
             System.out.println("0 - SALIR");
             System.out.println("Ingrese una opcion del menu");
             opcion = sc.nextInt();
-            switch(opcion){
-                case 1: crearEmpleado();
-                break;
-                case 2: modificarEmpleado();
-                break;
-                case 3: buscarEmpleado();
-                break;
-                case 4: listarEmpleado();
-                break;
-                default: 
+            switch (opcion) {
+                case 1:
+                    crearEmpleado();
+                    break;
+                case 2:
+                    modificarEmpleado();
+                    break;
+                case 3:
+                    buscarEmpleado();
+                    break;
+                case 4:
+                    listarEmpleado();
+                    break;
+                case 5:
+                    eliminarEmpleado();
+                default:
                     opcion = 0;
                     System.out.println("Gracias por usar este menu");
             }
-        }while(opcion != 0);
+        } while (opcion != 0);
     }
 
     private void crearEmpleado() {
         System.out.println("****CREAR EMPLEADOS****");
-        for(int i = 0; i < listaEmpleados.length; i++){
-            System.out.println("::CREAR EMPLEADO::");
-            System.out.println("Ingrese el documento del empleado");
-            String documento = sc.next();
-            // Validar documento acá tiene que estar mirar que no este repetido el empleado
-            System.out.println("Ingrese el nombre del empleado");
-            String nombre = sc.next();
-            System.out.println("Ingrese las horas trabajadas");
-            int horas  = sc.nextInt();
-            System.out.println("Ingrese el valor de la hora");
-            double valor = sc.nextDouble();
-            listaEmpleados[i] = new Empleado(documento, nombre, horas, valor, calcularSueldo(horas, valor));
-        }
-        
+
+        System.out.println("::CREAR EMPLEADO::");
+        System.out.println("Ingrese el documento del empleado");
+        String documento = sc.next();
+        // Validar documento acá tiene que estar mirar que no este repetido el empleado
+        System.out.println("Ingrese el nombre del empleado");
+        String nombre = sc.next();
+        System.out.println("Ingrese las horas trabajadas");
+        int horas = sc.nextInt();
+        System.out.println("Ingrese el valor de la hora");
+        double valor = sc.nextDouble();
+        listaEmpleados.add(new Empleado(documento, nombre, horas, valor, calcularSueldo(horas, valor)))
+
+
     }
 
     private void modificarEmpleado() {
         int posicion = buscarEmpleado();
-        if(posicion >= 0){
-            empleado = listaEmpleados[posicion];
+        if (posicion >= 0) {
+            empleado = listaEmpleados.get(posicion);
             System.out.println("::MODIFICAR EMPLEADO::");
             System.out.println("Ingrese el nombre del empleado");
             String nombre = sc.next();
             empleado.setNombreEmpleado(nombre);
             System.out.println("Ingrese las horas trabajadas");
-            int horas  = sc.nextInt();
+            int horas = sc.nextInt();
             empleado.setHorasTrabajadas(horas);
             System.out.println("Ingrese el valor de la hora");
             double valor = sc.nextDouble();
             empleado.setValorHora(valor);
             empleado.setSueldo(calcularSueldo(horas, valor));
-            listaEmpleados[posicion] = empleado;
+            listaEmpleados.set(posicion, empleado);
         }
 
     }
@@ -87,32 +91,51 @@ public class EmpleadoService {
         System.out.println("####Buscar empleado#####");
         System.out.println("Ingrese el numero de documento del empleado");
         String documento = sc.next();
-        for (int i=0; i<listaEmpleados.length; i++){
-            if(listaEmpleados[i].getDocumento().equals(documento)){
-                imprimirEmpleado(listaEmpleados[i]);
-                return i;
+        empleado = null;
+        for (Empleado empleadoEncontrado: listaEmpleados) {
+            if (empleadoEncontrado.getDocumento().equals(documento)) {
+                empleado = empleadoEncontrado;
+                imprimirEmpleado(empleadoEncontrado);
+                break;
             }
+
         }
+
         System.out.println("Registro no encontrado");
-        return -1;
+        return listaEmpleados.indexOf(empleado);
     }
 
     private void listarEmpleado() {
         System.out.println("#####Listado de empleados######");
-        for (int i=0; i<listaEmpleados.length; i++){
-            imprimirEmpleado(listaEmpleados[i]);
+        for (Empleado empleado: listaEmpleados) {
+            imprimirEmpleado(empleado);
         }
     }
+    private void eliminarEmpleado(){
+        System.out.println("Ingrese el documento del empleado");
+        String documento = sc.next();
+        for (Empleado deleteEmpleado: listaEmpleados) {
+            if (deleteEmpleado.getDocumento().equals(documento)) {
+                empleado = deleteEmpleado;
+                listaEmpleados.remove(empleado);
+                System.out.println("Empleado eliminado");
+                break;
+            }
 
-    private double calcularSueldo(int horas, double valorHora){
+        }
+        System.out.println("Empleado no encontrado");
+        return listaEmpleados.indexOf(empleado);
+    }
+
+    private double calcularSueldo(int horas, double valorHora) {
         return horas * valorHora;
     }
 
-    private void imprimirEmpleado(Empleado empleado){
+    private void imprimirEmpleado(Empleado empleado) {
         System.out.println(empleado.getDocumento() + " | "
-                + empleado.getNombreEmpleado() + " | " +empleado.getSueldo());
+                + empleado.getNombreEmpleado() + " | " + empleado.getSueldo());
     }
-    
+
 }
 
 
